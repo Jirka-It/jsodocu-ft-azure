11b916c8edf3fae57b181d08692ed1be3b97f07f
Jhonatan Jirka <gerencia@jirka.co>
2024-11-04 21:18:24 -0500

    [SODO] - [CHANGED]: update readme

--- next.config.js
+++ next.config.js
@@ -1,18 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output:"export",
+    basePath:"/cacao",
     images: {
         domains: ['dummyimage.com']
     },
     reactStrictMode: true,
-    async redirects() {
-        return [
-            {
-                source: '/apps/mail',
-                destination: '/apps/mail/inbox',
-                permanent: true
-            }
-        ];
-    }
 };
 
 module.exports = nextConfig;

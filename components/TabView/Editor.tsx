b3f315b539015441b528903fd379c5b63f44ac88
bomj321 <mauricio1314@hotmail.com>
2024-11-08 00:55:26 -0400

    [CHANGE] - Adding change mentions for value

--- components/TabView/Editor.tsx
+++ components/TabView/Editor.tsx
@@ -1,16 +1,54 @@
 import { Editor as Quill } from 'primereact/editor';
-import { useState } from 'react';
+import Mention from 'quill-mention';
+
+import { useEffect, useRef, useState } from 'react';
+
+const data = [
+    { id: 1, value: 'variable_PH', variable: 'Partha' },
+    { id: 2, value: 'date_contract', variable: 'Emma' }
+];
 
 export default function Editor() {
     const [text, setText] = useState('');
+    const quillRef = useRef<Quill | null>(null);
+    const quillLoaded = (event) => {
+        const quillInstance = event;
+
+        new Mention(quillInstance, {
+            mentionDenotationChars: ['@'],
+            source: function (searchTerm: string, renderList: (data: any, searchText: string) => void, mentionChar: string) {
+                // sample data set for displaying
+                // enter your logic here
+
+                renderList(data, searchTerm);
+            }
+        });
+    };
+
+    const replaceText = (text: string) => {
+        if (!text) {
+            return '';
+        }
+        let newString = text;
+
+        //Find by mentions
+        for (var i = 0; i < data.length; i++) {
+            const value = data[i].value;
+            const regex = new RegExp(`@${value}`, 'g');
+
+            newString = newString.replaceAll(regex, data[i].variable);
+        }
+
+        return newString;
+    };
+
     return (
         <section className="grid">
             <div className="col-12 md:col-6">
-                {' '}
-                <Quill value={text} onTextChange={(e) => setText(e.htmlValue)} style={{ height: '320px' }} />
+                <Quill ref={quillRef} value={text} onTextChange={(e) => setText(e.htmlValue)} style={{ height: '320px' }} onLoad={quillLoaded} />
             </div>
             <div className="col-12 md:col-6">
-                <div dangerouslySetInnerHTML={{ __html: text }}></div>
+                <div dangerouslySetInnerHTML={{ __html: replaceText(text) }}></div>
             </div>
         </section>
     );

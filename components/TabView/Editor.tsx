53ef6bf7b225142a2407638a246574d8c4047ae2
bomj321 <mauricio1314@hotmail.com>
2024-11-08 23:17:54 -0400

    [CHANGE] - Adding chatter menu

--- components/TabView/Editor.tsx
+++ components/TabView/Editor.tsx
@@ -1,5 +1,6 @@
 import { Editor as Quill } from 'primereact/editor';
 import Mention from 'quill-mention';
+import { Tree } from 'primereact/tree';
 
 import { useRef, useState } from 'react';
 
@@ -8,9 +9,26 @@ const data = [
     { id: 2, value: 'date_contract', variable: 'Emma' }
 ];
 
+const dataTree = [
+    {
+        key: '1',
+        label: 'Capítulo 1'
+    },
+    {
+        key: '2',
+        label: 'Capítulo 2'
+    },
+    {
+        key: '3',
+        label: 'Capítulo 3'
+    }
+];
+
 export default function Editor() {
     const [text, setText] = useState('');
     const quillRef = useRef<Quill | null>(null);
+    const [nodes, setNodes] = useState(dataTree);
+
     const quillLoaded = (event) => {
         const quillInstance = event;
 
@@ -44,11 +62,19 @@ export default function Editor() {
 
     return (
         <section className="grid">
-            <div className="col-12 md:col-6">
+            <div className="col-12 lg:col-2">
+                <h4 className="m-0">Conjunto Amatista</h4>
+                <h6 className="m-0 text-gray-500">Reglamento PH</h6>
+
+                <div className="mt-8 cursor-pointer text-blue-500">
+                    <i className="pi pi-plus-circle mr-3"></i> Agregar Capítulo
+                </div>
+            </div>
+            <div className="col-12 lg:col-5">
                 <Quill ref={quillRef} value={text} onTextChange={(e) => setText(e.htmlValue)} style={{ height: '320px' }} onLoad={quillLoaded} />
             </div>
-            <div className="col-12 md:col-6">
-                <div dangerouslySetInnerHTML={{ __html: replaceText(text) }}></div>
+            <div className="col-12 lg:col-5">
+                <div className="shadow-1 h-full pl-2 pr-2" dangerouslySetInnerHTML={{ __html: replaceText(text) }}></div>
             </div>
         </section>
     );

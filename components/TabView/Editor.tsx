219ffcf2ef1a0aa201581f05e93bac4225626952
bomj321 <mauricio1314@hotmail.com>
2024-11-09 03:33:11 -0400

    [CHANGE] - Updating array

--- components/TabView/Editor.tsx
+++ components/TabView/Editor.tsx
@@ -1,39 +1,19 @@
 import { Editor as Quill } from 'primereact/editor';
 import Mention from 'quill-mention';
 
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
 
 const data = [
     { id: 1, chatter: 'variable_PH', variable: 'Partha' },
+
     { id: 2, chatter: 'date_contract', variable: 'Emma' }
 ];
 
-const dataTree = [
-    {
-        id: '1',
-        label: 'Capítulo 1',
-        chatter: 'www',
-        content: 'wdwdwdwd'
-    },
-    {
-        id: '2',
-        label: 'Capítulo 2',
-        chatter: '',
-        content: ''
-    },
-    {
-        id: '3',
-        label: 'Capítulo 3',
-        chatter: '',
-        content: ''
-    }
-];
-
 export default function Editor() {
-    const [nodes, setNodes] = useState(dataTree);
-    const [contentSelected, setContentSelected] = useState<string>('1');
+    const [sections, setSections] = useState([]);
+    const [contentSelected, setContentSelected] = useState<number>();
 
     const quillLoaded = (event) => {
         const quillInstance = event;
@@ -49,6 +29,7 @@ export default function Editor() {
         });
     };
 
+    //Move it to utils
     const replaceText = (text: string) => {
         if (!text) {
             return '';
@@ -66,23 +47,40 @@ export default function Editor() {
         return newString;
     };
 
+    //Events to quill
     const handleInputChange = (id: string, value: any, key = 'chatter') => {
-        const modifiedNodes = nodes.map((v) => {
-            if (v.id === id) {
-                v[key] = value;
-            }
-            return v;
+        setSections((prevArray) => {
+            const modifiedSections = prevArray.map((v) => {
+                if (v.id === id) {
+                    v[key] = value;
+                }
+                return v;
+            });
+            return [...modifiedSections];
         });
-
-        setNodes(modifiedNodes);
     };
 
     const handleClickEvent = (id: string) => {
-        setContentSelected(id);
+        setContentSelected(parseInt(id));
     };
 
     const handleChangeEvent = (value: string, content) => {
         handleInputChange(content.id, value, 'content');
+    };
+
+    // Add chapter
+    const addChapter = () => {
+        setSections((prevArray) => [
+            ...prevArray,
+            {
+                id: sections.length + 1,
+                label: `Capítulo ${sections.length + 1}`,
+                chatter: '',
+                content: ''
+            }
+        ]);
+
+        setContentSelected(sections.length + 1);
     };
 
     return (
@@ -91,15 +89,14 @@ export default function Editor() {
                 <h4 className="m-0">Conjunto Amatista</h4>
                 <h6 className="m-0 text-gray-500 mb-5">Reglamento PH</h6>
                 <div>
-                    {' '}
-                    {nodes.map((d, i) => {
+                    {sections.map((s) => {
                         return (
-                            <div key={`${i}`}>
-                                <h5 className="m-0 mb-1 text-blue-500 font-bold cursor-pointer" onClick={() => handleClickEvent(d.id)}>
-                                    Capítulo {i + 1}.
+                            <div key={s.id}>
+                                <h5 className="m-0 mb-1 text-blue-500 font-bold cursor-pointer" onClick={() => handleClickEvent(s.id)}>
+                                    Capítulo {s.id}.
                                 </h5>
                                 <div className="flex">
-                                    <InputText value={d.chatter} onChange={(e) => handleInputChange(d.id, e.target.value)} className="w-full mb-3" type="text" placeholder="Nombre del capítulo" />
+                                    <InputText value={s.chatter} onChange={(e) => handleInputChange(s.id, e.target.value)} className="w-full mb-3" type="text" placeholder="Nombre del capítulo" />
                                     <Button icon="pi pi-save" rounded severity="info" aria-label="Delete" className="ml-2" tooltip="Guardar" />
                                     <Button icon="pi pi-times" rounded severity="danger" aria-label="Delete" className="ml-2" tooltip="Borrar" />
                                 </div>
@@ -107,18 +104,18 @@ export default function Editor() {
                         );
                     })}
                 </div>
-                <div className="mt-5 cursor-pointer text-blue-500">
+                <div className="mt-5 cursor-pointer text-blue-500" onClick={() => addChapter()}>
                     <i className="pi pi-plus-circle mr-3"></i> Agregar Capítulo
                 </div>
             </div>
-            {nodes.map((d, i) => {
+            {sections.map((s) => {
                 return (
-                    <div key={i} className={`grid col-12 lg:col-8 ${d.id !== contentSelected ? 'hidden' : ''}`}>
+                    <div key={s.id} className={`grid col-12 lg:col-8 ${s.id !== contentSelected ? 'hidden' : ''}`}>
                         <div className="col-12 lg:col-6">
-                            <Quill value={d.content} onTextChange={(e) => handleChangeEvent(e.htmlValue, d)} style={{ height: '320px' }} onLoad={quillLoaded} />
+                            <Quill value={s.content} onTextChange={(e) => handleChangeEvent(e.htmlValue, s)} style={{ height: '320px' }} onLoad={quillLoaded} />
                         </div>
                         <div className="col-12 lg:col-6">
-                            <div className="shadow-1 h-full pl-2 pr-2" dangerouslySetInnerHTML={{ __html: replaceText(d.content) }}></div>
+                            <div className="shadow-1 h-full pl-2 pr-2" dangerouslySetInnerHTML={{ __html: replaceText(s.content) }}></div>
                         </div>
                     </div>
                 );

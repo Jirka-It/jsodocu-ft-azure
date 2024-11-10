2be8f8cef65e85e33237fd102b4573351875de33
bomj321 <mauricio1314@hotmail.com>
2024-11-10 06:42:40 -0400

    [CHANGE] - Adding revision's view

--- components/TabView/Revision.tsx
+++ components/TabView/Revision.tsx
@@ -1,17 +1,38 @@
 import { useState } from 'react';
 import { Checkbox } from 'primereact/checkbox';
 import stylesRevision from './Revision.module.css';
+import { Dropdown } from 'primereact/dropdown';
+import { Button } from 'primereact/button';
+
+const names = [
+    { name: 'Juan Hernandez', code: 'CUSTOMER_ADMIN' },
+    { name: 'Maria Cortez', code: 'CUSTOMER' }
+];
 
 export default function Revision() {
+    const [name, setName] = useState<any>('');
     const [checked, setChecked] = useState(false);
+
+    const handleExport = () => {};
+
+    const handleSubmit = () => {};
 
     return (
         <section className="grid">
-            <div className="col-12 sm:col-4"></div>
-            <div className="col-12 sm:col-8">
-                {
-                    // flex justify-content-between
-                }
+            <div className="col-12 md:col-4">
+                <h3 className="m-0 mb-1 text-blue-500 font-bold cursor-pointer">Revisión</h3>
+
+                <div>
+                    <label htmlFor="name">Usuario asignado</label>
+                    <Dropdown value={name} onChange={(e) => setName(e.value)} options={names} id="name" optionLabel="name" placeholder="Usuario responsable" className="w-full mt-2" />
+                </div>
+
+                <div className="flex justify-content-center mt-5">
+                    <Button className="mr-3" label="Exportar" severity="danger" onClick={() => handleExport()} />
+                    <Button label="Enviar" onClick={() => handleSubmit()} />
+                </div>
+            </div>
+            <div className="col-12 md:col-8">
                 <div className={`${stylesRevision['header-editor']}`}>
                     <div className="header-editor__description">
                         <h4 className="m-0">Conjunto Amatista</h4>

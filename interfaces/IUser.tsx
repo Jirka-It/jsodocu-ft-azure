3c6f604489744c34fdf09cc70972c6cba9e5ca38
bomj321 <mauricio1314@hotmail.com>
2024-12-09 18:26:44 -0400

    [CHANGE] - Adding roles and permissions

--- interfaces/IUser.tsx
+++ interfaces/IUser.tsx
@@ -1,8 +1,18 @@
 export interface IUser {
     _id?: string;
+    username: string;
     name: string;
+    lastName: string;
     email: string;
     password: string;
     confirmPassword?: string;
     roles: Array<any>;
+}
+
+import { IGeneralRequest } from './IRequest';
+
+export interface IUserPartial extends Partial<IUser> {}
+
+export interface IUserResponse extends IGeneralRequest {
+    data: Array<IUser>;
 }

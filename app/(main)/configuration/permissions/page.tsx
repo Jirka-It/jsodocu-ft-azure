f3c9546b2ae1c228b3f688d4263a301e2743aa74
bomj321 <mauricio1314@hotmail.com>
2024-12-12 00:08:37 -0400

    [CHANGE] - Adding filter in roles and permissions

--- app/(main)/configuration/permissions/page.tsx
+++ app/(main)/configuration/permissions/page.tsx
@@ -9,16 +9,23 @@ import PermissionModal from '@components/Modals/PermissionModal';
 import DeleteModal from '@components/Modals/DeleteModal';
 import { Toast } from 'primereact/toast';
 import { IPermission, IPermissionResponse } from '@interfaces/IPermission';
+
 import { CopyToClipBoard } from '@lib/CopyToClipBoard';
 import { findAll, remove } from '@api/permissions';
 import { Badge } from 'primereact/badge';
+import { categories } from '@lib/data';
+import { InputText } from 'primereact/inputtext';
+import { showError } from '@lib/ToastMessages';
 
 const Permissions = () => {
     const toast = useRef(null);
+    const [timer, setTimer] = useState(null);
     const [openModal, setOpenModal] = useState<boolean>(false);
     const [openModalClose, setOpenModalClose] = useState<boolean>(false);
     const [tableState, setTableState] = useState<DataTableStateEvent>();
+    const [searchParam, setSearchParam] = useState<string>('');
     const [permission, setPermission] = useState<IPermission>(null);
+
     const [data, setData] = useState<IPermissionResponse>();
 
     useEffect(() => {
@@ -26,8 +33,9 @@ const Permissions = () => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
-    const getData = async (page: number = 1, size: number = data ? data?.elementsByPage : 10) => {
-        const res = await findAll({ page, size });
+    const getData = async (page: number = 1, size: number = data ? data?.elementsByPage : 10, search: string = '') => {
+        const param = search ? search : searchParam;
+        const res = await findAll({ page, size, searchParam: param });
         setData(res);
     };
 
@@ -60,6 +68,20 @@ const Permissions = () => {
         }
     };
 
+    const handleChange = async (searchParam: string) => {
+        setSearchParam(searchParam);
+        clearTimeout(timer);
+        const newTimer = setTimeout(async () => {
+            try {
+                getData(1, data?.elementsByPage, searchParam);
+            } catch (error) {
+                showError(toast, '', 'Contacte con soporte');
+            }
+        }, 1000);
+
+        setTimer(newTimer);
+    };
+
     return (
         <div className="layout-permissions">
             <Toast ref={toast} />
@@ -68,6 +90,8 @@ const Permissions = () => {
             <div className="card">
                 <div className="w-full flex justify-content-between mb-3">
                     <Button onClick={() => setOpenModal(true)} icon="pi pi-plus" className="mr-2" label="Permiso" />
+
+                    <InputText value={searchParam} onChange={(e) => handleChange(e.target.value)} id="searchParm" type="text" placeholder="Buscar" />
                 </div>
                 <DataTable
                     value={data?.data}
@@ -80,8 +104,9 @@ const Permissions = () => {
                     totalRecords={data?.elementsByPage * data?.totalPages}
                 >
                     <Column field="_id" header="Id" body={(rowData: IPermission) => <Badge onClick={() => handleCopy(rowData._id)} className="cursor-pointer text-lg" value={`${rowData._id.substr(-4)}`}></Badge>}></Column>
-                    <Column field="name" header="Nombre"></Column>
                     <Column field="code" header="Código"></Column>
+                    <Column field="name" header="Nombre"></Column>
+                    <Column field="category" header="Categoría" body={(rowData: IPermission) => `${categories.find((c) => c.code === rowData.category)?.name}`}></Column>
                     <Column field="description" header="Descripción"></Column>
                     <Column field="actions" body={(rowData) => <BasicActions handleEdit={() => handleEdit(rowData)} handleDelete={() => handleModalDelete(rowData)} />} header="Acciones"></Column>
                 </DataTable>
--- app/(main)/configuration/roles/page.tsx
+++ app/(main)/configuration/roles/page.tsx
@@ -16,11 +16,14 @@ import { findAll, update } from '@api/roles';
 import { CopyToClipBoard } from '@lib/CopyToClipBoard';
 import { Toast } from 'primereact/toast';
 import CustomTypeActions from '@components/TableExtensions/CustomTypeActions';
+import { InputText } from 'primereact/inputtext';
+import { showError } from '@lib/ToastMessages';
 
 const Roles = () => {
     const toast = useRef(null);
+    const [timer, setTimer] = useState(null);
     const [openModal, setOpenModal] = useState<boolean>(false);
-    //const [openModalClose, setOpenModalClose] = useState<boolean>(false);
+    const [searchParam, setSearchParam] = useState<string>('');
     const [checked, setChecked] = useState(true);
     const [tableState, setTableState] = useState<DataTableStateEvent>();
     const [rol, setRol] = useState<IRol>(null);
@@ -31,9 +34,10 @@ const Roles = () => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [checked]);
 
-    const getData = async (page: number = 1, size: number = data ? data?.elementsByPage : 10) => {
+    const getData = async (page: number = 1, size: number = data ? data?.elementsByPage : 10, search: string = '') => {
         const state = checked ? State.ACTIVE : State.INACTIVE;
-        const res = await findAll({ page, size, state });
+        const param = search ? search : searchParam;
+        const res = await findAll({ page, size, state, searchParam: param });
         setData(res);
     };
 
@@ -82,6 +86,20 @@ const Roles = () => {
         }
     };
 
+    const handleChange = async (searchParam: string) => {
+        setSearchParam(searchParam);
+        clearTimeout(timer);
+        const newTimer = setTimeout(async () => {
+            try {
+                getData(1, data?.elementsByPage, searchParam);
+            } catch (error) {
+                showError(toast, '', 'Contacte con soporte');
+            }
+        }, 1000);
+
+        setTimer(newTimer);
+    };
+
     return (
         <div className="layout-roles">
             <Toast ref={toast} />
@@ -91,7 +109,11 @@ const Roles = () => {
             <div className="card">
                 <div className="w-full flex justify-content-between mb-3">
                     <Button onClick={() => setOpenModal(true)} icon="pi pi-plus" className="mr-2" label="Rol" />
-                    <InputSwitch checked={checked} onChange={(e) => handleCheck(e.value)} />
+
+                    <div className="flex">
+                        <InputText value={searchParam} onChange={(e) => handleChange(e.target.value)} id="searchParm" className="mr-3" type="text" placeholder="Buscar" />
+                        <InputSwitch checked={checked} onChange={(e) => handleCheck(e.value)} />
+                    </div>
                 </div>
                 <DataTable
                     value={data?.data}
@@ -104,11 +126,10 @@ const Roles = () => {
                     totalRecords={data?.elementsByPage * data?.totalPages}
                 >
                     <Column field="_id" header="Id" body={(rowData: IRol) => <Badge onClick={() => handleCopy(rowData._id)} className="cursor-pointer text-lg" value={`${rowData._id.substr(-4)}`}></Badge>}></Column>
-                    <Column field="name" header="Nombre"></Column>
                     <Column field="code" header="Código"></Column>
+                    <Column field="name" header="Nombre"></Column>
                     <Column field="description" header="Descripción"></Column>
                     <Column field="applyToAccount" body={(rowData: IRol) => <BasicStates state={rowData.applyToAccount} />} header="Cuenta"></Column>
-                    <Column field="state" body={(rowData: IRol) => <BasicStates state={rowData.state} />} header="Estado"></Column>
                     <Column field="actions" body={(rowData: IRol) => <CustomTypeActions handleEdit={() => handleEdit(rowData)} data={rowData.state} handleDelete={() => handleDelete(rowData)} />} header="Acciones"></Column>
                 </DataTable>
             </div>

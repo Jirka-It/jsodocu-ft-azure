7c0c98dc928b88c12042c1aa9255763e17d343c6
bomj321 <mauricio1314@hotmail.com>
2025-01-07 18:11:35 -0400

    [CHANGE] - Adding soft fix in variables

--- lib/ReplaceText.tsx
+++ lib/ReplaceText.tsx
@@ -28,6 +28,10 @@ export const replaceTextQuill = (text: string, variables: Array<IVariableLight>)
     return newString;
 };
 
+function escapeRegExp(str) {
+    return str + '(?![_,-, ,*,=])';
+}
+
 export const replaceText = (text: string, variables: Array<IVariableLight>) => {
     if (!text || !variables) {
         return '';
@@ -37,7 +41,20 @@ export const replaceText = (text: string, variables: Array<IVariableLight>) => {
     //Find by mentions
     for (var i = 0; i < variables.length; i++) {
         const valueMention = variables[i].value;
-        const regex = new RegExp(`@${valueMention}`, 'g');
+
+        // const regex = new RegExp('^@' + valueMention + '$', 'g');
+
+        const regex = new RegExp(escapeRegExp(`@${valueMention}`), 'g');
+
+        //console.log('-------------------------------');
+
+        //console.log('valueMention', valueMention);
+        //console.log('variables[i].name', variables[i].name);
+        // console.log('variables[i].value', variables[i].value);
+
+        //const found = newString.match(regex);
+
+        //console.log('found', found);
 
         newString = newString.replaceAll(regex, variables[i].name);
     }
